0b8b1e946087dce8342037257a3b3ce9975f81ba
mijkolsmith <mijkolsmith@live.com>
2020-11-09 15:30:46 +0100

    added a bit of the boid implementation

---
+++ K2-AI/Assets/Scripts/Boid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid
+{
+	Vector3 v1, v2, v3;
+	GameObject boid;
+	Vector3 Position;
+    
+	public Boid(GameObject _boidPrefab)
+	{
+		Position = new Vector3(0, 0, 0);
+		Object.Instantiate(_boidPrefab, Position, Quaternion.identity);
+		boid = _boidPrefab;
+	}
+
+	public void UpdateBoid(Vector3 a, Vector3 b, Vector3 c)
+	{
+		//rule 1: centre of mass
+		Position = boid.transform.localPosition;
+		a -= Position;
+		Vector3 centreOfMass = a / (GameManager.Instance.boidCount - 1);
+
+		v1 = centreOfMass;
+
+		//rule 2: social distancing
+		v2 = b;
+
+		//rule 3: match velocity
+		v3 = c;
+	}
+
+	public Vector3 GetPosition()
+	{
+		return Position;
+	}
+}
---
+++ K2-AI/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	Dictionary<int, Boid> boids;
+	public GameObject boidPrefab;
+	public int boidCount = 20;
+	
+    void Start()
+    {
+		if (instance != null && instance != this)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(this);
+
+		boids = new Dictionary<int, Boid>();
+		for (int i = 0; i < boidCount; i++)
+		{
+			boids.Add(i, new Boid(boidPrefab));
+		}
+	}
+
+    void Update()
+    {
+		Vector3 a = new Vector3(0, 0, 0);
+		Vector3 b = new Vector3(0, 0, 0);
+		Vector3 c = new Vector3(0, 0, 0);
+
+		for (int i = 0; i < boidCount; i++)
+		{
+			a += boids[i].GetPosition();
+		}
+
+		for (int i = 0; i < boidCount; i++)
+		{
+			for (int j = 0; j < boidCount; j++)
+			{
+				if (boids[i] != boids[j])
+				{
+					if (Mathf.Abs(boids[i].GetPosition().x + boids[j].GetPosition().x) < 10 || Mathf.Abs(boids[i].GetPosition().y + boids[j].GetPosition().y) < 10 || Mathf.Abs(boids[i].GetPosition().z + boids[j].GetPosition().z) < 10)
+					{
+						b = b - (boids[i].GetPosition() + boids[j].GetPosition());
+					}
+				}
+			}
+
+			boids[i].UpdateBoid(a, b, c);
+		}
+	}
+}
